ce21608fa077c13902503fdf5396e2df36654bb8
Pawel <p.sznura@rgu.ac.uk>
2023-01-26 14:39:26 +0000

    server is working, added 404 error

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var app = express();
+
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
-    res.send("Hello world! by express");
+    // res.send("Hello world! by express");
+    res.redirect('index.html')
 });
+
+
+// app.use(function(req, res, next){
+//     res.status(404);
+  
+//     res.format({
+//       html: function () {
+//         res.render('404', { url: req.url })
+//       },
+//       json: function () {
+//         res.json({ error: 'Not found' })
+//       },
+//       default: function () {
+//         res.type('txt').send('Not found')
+//       }
+//     })
+//   });
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res){
+    res.status(404).send('Error 404');
+  });
+
 app.listen(8080)
 
 console.log(`Server is listening for requests on port 8080`);
